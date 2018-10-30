5bf4ea45bd9aae48fce4a6fdcfa9c7ba7f898461
Pedro Ferreira Morais <pedro.fr.morais@gmail.com>
2018-10-30 10:44:41 -0300

    Further testing

--- src/Testes.py
+++ src/Testes.py
@@ -24,11 +24,19 @@ def filtro_branco(img1):
     kernel = np.ones((2,2),np.uint8)
     opening = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
 
+    print(type(opening))
+
+    #threshold to binary
+    cv2.threshold(opening, 100, 255, cv2.THRESH_BINARY, opening)
+
+    print(type(opening))
+
     return opening
 
 def vertice3(img1):
 
     data = vertices(img1)
+    cv2.imwrite('data.png', data)
     xy = peak_local_max(data, min_distance=2,threshold_abs=1500)
     # print(xy)
 
@@ -39,7 +47,6 @@ def vertice3(img1):
         black[pos[0]][pos[1]] = 255
 
     cv2.imwrite('result.png', black)
-    cv2.imwrite('data.png', data)
 
 def vertice2(img1):
     neighborhood_size = 5
@@ -74,7 +81,9 @@ def vertices(img1):
     img = img1.copy()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     gray = np.float32(gray)
-    dst = cv2.cornerHarris(gray,4,3,0.06)
+    dst = cv2.cornerHarris(gray,5,5,0.1)
+
+    cv2.imwrite('dst.png', dst)
     
     #result is dilated for marking the corners, not important
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
@@ -108,6 +117,10 @@ filename = '../img/caixa5.jpg'
 img = cv2.imread(filename)
 
 img = filtro_branco(img)
+
+cv2.imwrite('non_resized.png', img)
+img = cv2.resize(img, (0,0), fx=2, fy=2, interpolation=cv2.INTER_NEAREST)
+cv2.imwrite('resized.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 img = vertice3(img)
 
 if cv2.waitKey(0) & 0xff == 27:
